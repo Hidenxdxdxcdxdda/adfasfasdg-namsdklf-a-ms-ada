9561b6c50f726d5b362fb74a00c5c62fab486fcb
Hidenxdxdxcdxdda <35894515+Hidenxdxdxcdxdda@users.noreply.github.com>
2018-01-28 12:31:40 -0200

    Add files via upload

---
+++ dog.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js');
+
+//Defined it as client
+const client = new Discord.Client();
+
+const config = require('./config.json');
+
+//Insert bot prefix
+const prefix = '=='
+
+//Install snekfetch (npm install snekfetch)
+const {get} = require("snekfetch"); 
+
+client.on('ready', () =>{
+	console.log('I am ready!')
+});
+
+
+client.on('message', message => {
+	if (message.content.startsWith(prefix + 'cat')) {
+		try {
+			get('https://random.cat/meow').then(response => {
+				message.channel.send({files: [{attachment: response.body.file, name: `cat.${response.body.file.split('.')[2]}`}]});
+			})
+		} catch (e) {
+			console.log(e);
+		}
+	};
+});
+
+//Login to discord
+client.login('process.env.BOT_TOKEN');
